a296a12c8c6de18f4c7ab426323ae35bb4a008d4
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-03-15 19:51:29 -0700

    minor

--- boost_source/build.sh
+++ boost_source/build.sh
@@ -4,6 +4,8 @@ mkdir -p $PREFIX/src/boost
 mv boost $PREFIX/src/boost/
 mv libs $PREFIX/src/boost/
 
+mkdir $PREFIX/bin
+
 cp $RECIPE_DIR/get_boost_sources $PREFIX/bin/
 chmod a+x $PREFIX/bin/get_boost_sources
 
