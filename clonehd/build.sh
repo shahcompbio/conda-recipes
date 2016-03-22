ed7352a503180c1e0c3fc81efc4d3c93ea6aef3f
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-03-21 17:22:52 -0700

    debug symbols for clonehd

--- clonehd/build.sh
+++ clonehd/build.sh
@@ -4,7 +4,7 @@ mkdir build
 
 cd src
 export CPLUS_INCLUDE_PATH=$PREFIX/include
-make "GSL=$PREFIX/lib/libgsl.a $PREFIX/lib/libgslcblas.a" CC=g++
+make "GSL=$PREFIX/lib/libgsl.a $PREFIX/lib/libgslcblas.a" CC=g++ CC_FLAGS="-Wall -ggdb -O3 -fopenmp -DHAVE_INLINE"
 cd ..
 
 mkdir -p $PREFIX/bin
