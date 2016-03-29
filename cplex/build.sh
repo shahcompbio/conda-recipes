f8bccafb1def0da4ce658bf3fc6e02656c8ddf69
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-03-29 13:50:39 -0700

    cplex

--- cplex/build.sh
+++ cplex/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo -e "2\n\n1\n$PREFIX/ibm/ilog/cplex_studio1251\nY\n\n\n" | bash cplex_studio1251.linux-x86-64.bin
+TEMP_DIR=`pwd`/tmp
+echo -e "2\n\n1\n$TEMP_DIR\nY\n\n\n\n" | bash cplex_studio1251.linux-x86-64.bin
+
+mkdir -p $PREFIX/include
+cp --recursive $TEMP_DIR/concert/include/ilconcert $PREFIX/include/
+cp --recursive $TEMP_DIR/cplex/include/ilcplex $PREFIX/include/
+
+mkdir -p $PREFIX/lib
+cp $TEMP_DIR/concert/lib/*/static_pic/lib* $PREFIX/lib/
+cp $TEMP_DIR/cplex/lib/*/static_pic/lib* $PREFIX/lib/
