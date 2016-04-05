8cb20b4b19428d1e5cfdd877a2090e37c7966fb0
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-04-05 13:31:12 -0700

    biobambam2 working

--- biobambam2/build.sh
+++ biobambam2/build.sh
@@ -2,6 +2,10 @@
 
 export CPPFLAGS="-I${PREFIX}/include/"
 export LDFLAGS="-L${PREFIX}/lib/"
+export CC="/usr/local/bin/gcc"
+export CXX="/usr/local/bin/g++"
+export CPP="/usr/local/bin/cpp"
+export CXXCPP="/usr/local/bin/cpp"
 
 ./configure --prefix=$PREFIX
 make
--- libmaus2/build.sh
+++ libmaus2/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 export CPPFLAGS="-I${PREFIX}/include/"
-export CFLAGS="-arch i386 -arch x86_64 -O2"
-export CXXFLAGS="-arch i386 -arch x86_64 -O2"
+export CFLAGS="-arch x86_64 -O2"
+export CXXFLAGS="-arch x86_64 -O2"
 export LDFLAGS="-L${PREFIX}/lib/"
-export LIBS="-arch i386 -arch x86_64"
+export LIBS="-arch x86_64"
+export CC="/usr/local/bin/gcc"
+export CXX="/usr/local/bin/g++"
+export CPP="/usr/local/bin/cpp"
+export CXXCPP="/usr/local/bin/cpp"
 bash configure --prefix=$PREFIX --disable-shared-libmaus2 --disable-asm \
-    --disable-dependency-tracking
+    --disable-dependency-tracking --enable-compile-testprograms=no
 make install
