d0ad92952ba89bf52776706ea1386516e186f670
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-05-04 14:19:24 -0700

    linux binary from github

--- delly/build.sh
+++ delly/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/lib
 
-export INCLUDE_PATH=$PREFIX/include
-export C_INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
-export LIBRARY_PATH=$PREFIX/lib
-
-cp $RECIPE_DIR/Makefile ./
-make STATIC=1 PARALLEL=0 -B src/delly
-cp src/delly $PREFIX/bin/delly
+cp delly $PREFIX/bin
+chmod a+x $PREFIX/bin/delly
