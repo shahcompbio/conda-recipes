5e336eb544a85eeb12b696b55cf17d894639254e
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-05-19 13:06:15 -0700

    delly 0.7.3 on osx

--- delly/build.sh
+++ delly/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/bin
 
-cp delly $PREFIX/bin
-chmod a+x $PREFIX/bin/delly
+export INCLUDE_PATH=$PREFIX/include
+export C_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+export LIBRARY_PATH=$PREFIX/lib
+
+cp $RECIPE_DIR/Makefile ./
+make STATIC=0 PARALLEL=0 -B src/delly
+cp src/delly $PREFIX/bin/delly
+
