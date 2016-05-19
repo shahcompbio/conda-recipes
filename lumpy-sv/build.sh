37a2ea3e87a669431e5e10c50ce47bccae471847
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-05-19 15:02:07 -0700

    lumpy sv updated

--- lumpy-sv/build.sh
+++ lumpy-sv/build.sh
@@ -5,4 +5,22 @@ mkdir -p $PREFIX/bin
 make
 cp bin/lumpy $PREFIX/bin/lumpy
 cp bin/lumpyexpress $PREFIX/bin/lumpyexpress
-cp bin/lumpyexpress.config $PREFIX/bin/lumpyexpress.config
+cp scripts/extractSplitReads_BwaMem $PREFIX/bin/lumpy_extractSplitReads_BwaMem
+
+mkdir -p $PREFIX/opt/lumpy
+cp -R scripts $PREFIX/opt/lumpy/
+
+echo "
+LUMPY_HOME=$PREFIX/opt/lumpy/
+
+LUMPY=$PREFIX/bin/lumpy
+SAMBLASTER=$PREFIX/bin/samblaster
+SAMBAMBA=$PREFIX/bin/sambamba
+SAMTOOLS=$PREFIX/bin/samtools
+PYTHON=$PREFIX/bin/python
+
+PAIREND_DISTRO=$PREFIX/opt/lumpy/scripts/pairend_distro.py
+BAMGROUPREADS=$PREFIX/opt/lumpy/scripts/bamkit/bamgroupreads.py
+BAMFILTERRG=$PREFIX/opt/lumpy/scripts/bamkit/bamfilterrg.py
+BAMLIBS=$PREFIX/opt/lumpy/scripts/bamkit/bamlibs.py
+" >> $PREFIX/bin/lumpyexpress.config
