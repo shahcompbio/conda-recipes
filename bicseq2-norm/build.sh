6434324413d52045edc884e4c6fb387a1985e83a
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-08-10 14:03:15 -0700

    argv for bicseq

--- bicseq2-norm/build.sh
+++ bicseq2-norm/build.sh
@@ -5,5 +5,5 @@ mkdir -p $PREFIX/src/bicseq2-norm/
 cp -R * $PREFIX/src/bicseq2-norm/
 
 mkdir -p $PREFIX/bin
-printf "#!/bin/bash\ncd $PREFIX/src/bicseq2-norm/ && ./NBICseq-norm.pl\n" > $PREFIX/bin/bicseq2-norm
+printf "#!/bin/bash\ncd $PREFIX/src/bicseq2-norm/ && ./NBICseq-norm.pl \$@\n" > $PREFIX/bin/bicseq2-norm
 chmod a+x $PREFIX/bin/bicseq2-norm
--- bicseq2-seg/build.sh
+++ bicseq2-seg/build.sh
@@ -5,5 +5,5 @@ mkdir -p $PREFIX/src/bicseq2-seg/
 cp -R * $PREFIX/src/bicseq2-seg/
 
 mkdir -p $PREFIX/bin
-printf "#!/bin/bash\ncd $PREFIX/src/bicseq2-seg/ && ./NBICseq-seg.pl\n" > $PREFIX/bin/bicseq2-seg
+printf "#!/bin/bash\ncd $PREFIX/src/bicseq2-seg/ && ./NBICseq-seg.pl \$@\n" > $PREFIX/bin/bicseq2-seg
 chmod a+x $PREFIX/bin/bicseq2-seg
