e55d1bce841358c86bb23171ff535540152c679a
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-08-11 12:48:35 -0700

    theta and bnpy dep

---
+++ theta/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+bash -e ./install
+
+mkdir -p $PREFIX/src/theta/
+cp -R * $PREFIX/src/theta/
+
+mkdir -p $PREFIX/bin
+
+printf "#!/bin/bash\ncd $PREFIX/src/theta/ && ./bin/RunTHetA \$@\n" > $PREFIX/bin/RunTHetA
+chmod a+x $PREFIX/bin/RunTHetA
+
+printf "#!/bin/bash\njava -jar $PREFIX/src/theta/runBICSeqToTHetA.jar \$@\n" > $PREFIX/bin/BICSeqToTHetA
+chmod a+x $PREFIX/bin/BICSeqToTHetA
