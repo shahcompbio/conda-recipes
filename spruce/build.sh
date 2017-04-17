609efa768f93f39a5c1b15150d6f68dbaf58121a
Andrew McPherson <andrew.mcpherson@gmail.com>
2017-04-17 16:05:28 -0700

    copy spruce executables

--- spruce/build.sh
+++ spruce/build.sh
@@ -8,3 +8,10 @@ mkdir build
 cd build
 cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 ..
 make
+
+mv cliques ${PREFIX}/bin/spruce_cliques
+mv enumerate ${PREFIX}/bin/spruce_enumerate
+mv merge ${PREFIX}/bin/spruce_merge
+mv rank ${PREFIX}/bin/spruce_rank
+mv visualize ${PREFIX}/bin/spruce_visualize
+
