c59b353d2c6446619793b10d03b4f39ba7f6d37e
Andrew McPherson <andrew.mcpherson@gmail.com>
2017-04-17 15:52:46 -0700

    spruce recipe

--- spruce/build.sh
+++ spruce/build.sh
@@ -2,8 +2,9 @@
 
 export BOOST_ROOT=${PREFIX}/include/
 export CPLUS_INCLUDE_PATH=${PREFIX}/lemon/include/
+export LIBRARY_PATH=${PREFIX}/lemon/lib/
 
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 ..
 make
