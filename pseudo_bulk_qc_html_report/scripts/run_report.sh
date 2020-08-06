34b1aa3078c35d722bfff6881881f33a5cf00f01
Douglas Abrams <abramsd@ceto.cbio.private>
2020-08-06 12:23:12 -0400

    single cell qc conda recipe

---
+++ pseudo_bulk_qc_html_report/scripts/run_report.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+R -e "rmarkdown::render('$DIR/report.Rmd',output_file = '$1',  params=list(sample_id='$2', mutations_per_cell_png='$3', summary_csv='$4', snvs_high_impact_csv='$5', snvs_all_csv='$6', trinuc_csv='$7', snv_adjacent_distance_png='$8', snv_genome_count_png='$9', snv_cell_counts_png='${10}', snv_alt_counts_png='${11}', rearranegementtype_distribution_png='${12}', chromosome_types_png='${13}', BAFplot_png='${14}', cn_plot_png='${15}', datatype_summary_csv='${16}', maf='${17}'))"
+
