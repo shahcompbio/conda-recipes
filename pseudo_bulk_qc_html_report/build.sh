a9db6058e5f4ae1c733a5957eb8d23be9d401bb1
Douglas Abrams <abramsd@ceto.cbio.private>
2020-08-20 15:04:20 -0400

    bug fix

--- pseudo_bulk_qc_html_report/build.sh
+++ pseudo_bulk_qc_html_report/build.sh
@@ -3,7 +3,6 @@
 
 # Install custom scripts for running qc
 mkdir -p $PREFIX/bin
-cp $RECIPE_DIR/scripts/filtermafs.R $PREFIX/bin
 cp $RECIPE_DIR/scripts/mergemafs.R $PREFIX/bin
 cp $RECIPE_DIR/scripts/mergesnvs.R $PREFIX/bin
 cp $RECIPE_DIR/scripts/mutationreport.Rmd $PREFIX/bin
@@ -11,7 +10,6 @@ cp $RECIPE_DIR/scripts/report.Rmd $PREFIX/bin
 cp $RECIPE_DIR/scripts/run_report.sh $PREFIX/bin
 cp $RECIPE_DIR/scripts/run_mutationreport.sh $PREFIX/bin
 
-chmod a+x $PREFIX/bin/filtermafs.R
 chmod a+x $PREFIX/bin/mergemafs.R
 chmod a+x $PREFIX/bin/mergesnvs.R
 chmod a+x $PREFIX/bin/mutationreport.Rmd
