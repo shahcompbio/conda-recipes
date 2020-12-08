0ba738ad420a2f6e745ee58a4c64e8ed1769d5bd
DouglasAbrams <32776088+DouglasAbrams@users.noreply.github.com>
2020-12-08 14:27:40 -0500

    cna annotation + germline/somatic labeling (#8)
    Co-authored-by: Douglas Abrams <abramsd@ceto.cbio.private>

--- wgs_qc_html_report/build.sh
+++ wgs_qc_html_report/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+mv DESCRIPTION DESCRIPTION.old
+grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+
+R CMD INSTALL --build .
 
 # Install custom scripts for running qc
 mkdir -p $PREFIX/bin
-cp $RECIPE_DIR/scripts/report.Rmd $PREFIX/bin
-cp $RECIPE_DIR/scripts/run_report.sh $PREFIX/bin
-cp $RECIPE_DIR/scripts/plots.R $PREFIX/bin
+cp $RECIPE_DIR/scripts/cohort_qc_report.Rmd $PREFIX/bin
+cp $RECIPE_DIR/scripts/run_cohort_qc_report.sh $PREFIX/bin
+cp $RECIPE_DIR/scripts/maftools_plots.R $PREFIX/bin
 
-chmod a+x $PREFIX/bin/report.Rmd
-chmod a+x $PREFIX/bin/run_report.sh
-chmod a+x $PREFIX/bin/plots.R
+chmod a+x $PREFIX/bin/cohort_qc_report.Rmd
+chmod a+x $PREFIX/bin/run_cohort_qc_report.sh
+chmod a+x $PREFIX/bin/maftools_plots.R
+
