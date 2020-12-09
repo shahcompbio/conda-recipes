c21b8ad0fce873d34a9c035f89df02f6952035b0
DouglasAbrams <32776088+DouglasAbrams@users.noreply.github.com>
2020-12-09 16:30:41 -0500

    oncogenic class added to variant class (#9)
    Co-authored-by: Douglas Abrams <abramsd@ceto.cbio.private>

--- wgs_qc_html_report/scripts/maftools_plots.R
+++ wgs_qc_html_report/scripts/maftools_plots.R
@@ -28,23 +28,24 @@ main = function(){
     genes=c("PPM1D", "TP53",  "BRCA1", "BRCA2", "MECOM", "RB1", "PTEN", "PALB2","ERBB2", "CDK12", "PIK3CA", "KRAS", "CCNE1", "MYC")
 
     maf_file = args[1]
-    cn=args[2]
-    filtered_maf_file = args[3]
-    oncoplot_path = args[4]
+    vcNames=args[2]
+    cn=args[3]
+    
     somatic_interactions = args[5]
 
     mafsummary = args[6]
-    vcNames = c("Frame_Shift_Del_somatic", "Frame_Shift_Ins_somatic", "Splice_Site_somatic", "Translation_Start_Site_somatic",
-"Nonsense_Mutation_somatic", "Nonstop_Mutation_somatic", "In_Frame_Del_somatic","In_Frame_Ins_somatic", "Missense_Mutation_somatic",
-"Frame_Shift_Del_germline", "Frame_Shift_Ins_germline", "Splice_Site_germline", "Translation_Start_Site_germline",
-"Nonsense_Mutation_germline", "Nonstop_Mutation_germline", "In_Frame_Del_germline","In_Frame_Ins_germline", "Missense_Mutation_germline")
-    unfiltmaf = maftools::read.maf(maf=maf_file, cnTable=cn, vc_nonSyn=vcNames)
-    filtmaf = maftools::read.maf(maf=filtered_maf_file, cnTable=cn, vc_nonSyn=vcNames)
+
+    vcNames=read.table(vcNames,header=TRUE)$Variant_Classification
+
+    maf = maftools::read.maf(maf=maf_file, cnTable=cn, vc_nonSyn=vcNames)
     
-    oncoplot(filtmaf, oncoplot_path, genes)
-    somatic_interaction_plot(unfiltmaf, somatic_interactions)
-    maf_summary(unfiltmaf, mafsummary)
+    oncoplot(maf, oncoplot_path, genes)
+    maf_summary(,af, mafsummary)
+
+    somatic_interaction_plot(maf, somatic_interactions)
+
 
 }
 
 main()
+
